832737432d18f9d2b2fd31657d7d76c89018eb9b
Ajay Prathiv A S <sivaaki05@gmail.com>
2026-02-13 10:11:09 +0530

    Refactor method to find string matches in array

--- StringMatchinginArray.java
+++ StringMatchinginArray.java
@@ -1,40 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public List<String> findAndReplacePattern(String[] words, String pattern) {
+    public List<String> stringMatching(String[] words) {
         List<String> result = new ArrayList<>();
 
-        for (String word : words) {
-            if (matches(word, pattern)) {
-                result.add(word);
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && words[j].contains(words[i])) {
+                    result.add(words[i]);
+                    break;
+                }
             }
         }
 
         return result;
-    }
-
-    private boolean matches(String word, String pattern) {
-        if (word.length() != pattern.length()) {
-            return false;
-        }
-
-        int[] map1 = new int[26];
-        int[] map2 = new int[26];
-        Arrays.fill(map1, -1);
-        Arrays.fill(map2, -1);
-
-        for (int i = 0; i < word.length(); i++) {
-            int c1 = word.charAt(i) - 'a';
-            int c2 = pattern.charAt(i) - 'a';
-
-            if (map1[c1] == -1 && map2[c2] == -1) {
-                map1[c1] = c2;
-                map2[c2] = c1;
-            } else if (map1[c1] != c2 || map2[c2] != c1) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
